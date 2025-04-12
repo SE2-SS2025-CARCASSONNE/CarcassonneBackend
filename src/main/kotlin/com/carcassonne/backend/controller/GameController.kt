1f70887c73e880a6f760eb324d732e889ca2573c
Felix Brunnbauer <februnnbauer@edu.aau.at@users.noreply.github.com>
2025-04-13 01:37:19 +0200

    GameController : Create Game request -> hostname convert datatype from string to Player , import model.player , GameMessage -> nullable Player datatype with default value null -> not nullable datatype

--- src/main/kotlin/com/carcassonne/backend/controller/GameController.kt
+++ src/main/kotlin/com/carcassonne/backend/controller/GameController.kt
@@ -2,6 +2,7 @@ package com.carcassonne.backend.controller
 
 import com.carcassonne.backend.entity.Game
 import com.carcassonne.backend.model.Tile
+import com.carcassonne.backend.model.Player
 import com.carcassonne.backend.repository.GameRepository
 import com.carcassonne.backend.service.GameManager
 import io.swagger.v3.oas.annotations.Operation
@@ -19,7 +20,7 @@ class GameController(
     private val gameManager: GameManager
 ) {
 
-    data class CreateGameRequest(val playerCount: Int, val hostName: String)
+    data class CreateGameRequest(val playerCount: Int, val hostName: Player)
     data class CreateGameResponse(val gameId: String)
 
     @Operation(summary = "Ping the server")
