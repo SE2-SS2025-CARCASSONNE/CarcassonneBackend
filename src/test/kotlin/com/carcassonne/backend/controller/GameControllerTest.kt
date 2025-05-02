fffad45adff110ad33dfb636e247bdd586953692
almin <alminkadiric7@gmail.com>
2025-05-03 00:54:02 +0200

    refactor: clean up by deleting unused commented code

--- src/test/kotlin/com/carcassonne/backend/controller/GameControllerTest.kt
+++ src/test/kotlin/com/carcassonne/backend/controller/GameControllerTest.kt
@@ -10,52 +10,6 @@ import org.mockito.Mockito.*
 import java.time.Instant
 
 class GameControllerTest {
-    /*
 
-    private lateinit var gameRepository: GameRepository
-    private lateinit var gameController: GameController
-
-    @BeforeEach
-    fun setUp() {
-        //Mock GameRepository dependency to avoid using real DB
-        gameRepository = mock(GameRepository::class.java)
-        //Pass mocked dependency to GameController
-        gameController = GameController(gameRepository)
-    }
-
-    @Test
-    //Server should return "pong" when pinged
-    fun pingServerTest() {
-        val result = gameController.ping()
-        assertEquals("pong", result)
-    }
-
-    @Test
-    //createGame should return gameId and save game in repository
-    fun createGameTest() {
-        val request = GameController.CreateGameRequest(playerCount = 3)
-        val response = gameController.createGame(request)
-
-        assertNotNull(response.gameId)
-        assertEquals(6, response.gameId.length)
-        verify(gameRepository, times(1)).save(any(Game::class.java))
-    }
-
-    @Test
-    //createGame should save game data in repository correctly
-    fun createGameCorrectSaveTest() {
-        val request = GameController.CreateGameRequest(playerCount = 2)
-        val captor = ArgumentCaptor.forClass(Game::class.java)
-
-        gameController.createGame(request)
-        verify(gameRepository).save(captor.capture())
-
-        val gameSave = captor.value
-        assertEquals(6, gameSave.gameCode.length)
-        assertEquals("WAITING", gameSave.status)
-        assertNotNull(gameSave.createdAt)
-        assertTrue(gameSave.createdAt.isBefore((Instant.now()).plusSeconds(1)))
-    }
-    */
 
 }
